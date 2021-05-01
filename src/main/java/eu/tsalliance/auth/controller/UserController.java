e49ebe3f0a7d236c39d2d846dedb62d9705e0a0f
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-05-02 00:29:36 +0200

    added permissions

--- src/main/java/eu/tsalliance/auth/controller/UserController.java
+++ src/main/java/eu/tsalliance/auth/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,31 +17,31 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(path = "{id}", method = RequestMethod.GET)
-    //@PreAuthorize("hasAuthority('alliance.users.read')")
+    @PreAuthorize("hasAuthority('alliance.users.read')")
     public ResponseEntity<User> getUser(@PathVariable("id") String id) {
         return ResponseEntity.of(this.userService.findUserById(id));
     }
 
     @GetMapping
-    //@PreAuthorize("hasAuthority('alliance.users.read')")
+    @PreAuthorize("hasAuthority('alliance.users.read')")
     public Page<User> getUsers(Pageable pageable) {
         return this.userService.findUsers(pageable);
     }
 
     @PostMapping
-    //@PreAuthorize("hasAuthority('alliance.users.write')")
+    @PreAuthorize("hasAuthority('alliance.users.write')")
     public User createUser(@RequestBody User user) throws Exception {
         return this.userService.createUser(user, true);
     }
 
     @PutMapping("{id}")
-    //@PreAuthorize("hasAuthority('alliance.users.write')")
+    @PreAuthorize("hasAuthority('alliance.users.write')")
     public User updateUser(@PathVariable("id") String id, @RequestBody User user) throws Exception {
         return this.userService.updateUser(id, user);
     }
 
     @DeleteMapping("{id}")
-    //@PreAuthorize("hasAuthority('alliance.users.write')")
+    @PreAuthorize("hasAuthority('alliance.users.write')")
     public void deleteUser(@PathVariable("id") String id) {
         this.userService.deleteUser(id);
     }
