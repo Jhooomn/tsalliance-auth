f3b9c4d461962fb0d054f8bfcf40b2175ce7896f
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-05-01 10:47:41 +0200

    added url validation

---
+++ src/main/java/eu/tsalliance/auth/validator/rules/UrlRule.java
@@ -0,0 +1,29 @@
+package eu.tsalliance.auth.validator.rules;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class UrlRule extends ValidationRule<String, UrlRule> {
+
+    public UrlRule(String fieldname, String subject, boolean required, boolean throwException) {
+        super(fieldname, subject, required, throwException);
+    }
+
+    @Override
+    public void test() {
+        Pattern pattern = Pattern.compile("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)");
+
+        if(!this.getSubject().matches(pattern.pattern())) {
+            putFailedTest("url", false, true);
+        }
+    }
+
+    @Override
+    public Map<String, Object> getRequirements() {
+        Map<String, Object> requirements = new HashMap<>();
+        requirements.put("url", true);
+        requirements.put("required", this.isRequired());
+        return requirements;
+    }
+}
