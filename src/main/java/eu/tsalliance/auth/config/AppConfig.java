5818689af33beced5baef321ef464f3cdc6d48d4
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-05-08 22:22:12 +0200

    fixed

--- src/main/java/eu/tsalliance/auth/config/AppConfig.java
+++ src/main/java/eu/tsalliance/auth/config/AppConfig.java
@@ -3,14 +3,13 @@ package eu.tsalliance.auth.config;
 import eu.tsalliance.apiutils.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    @Scope(value = WebApplicationContext.SCOPE_REQUEST)
+    @RequestScope
     public Validator validator() {
         return new Validator();
     }
